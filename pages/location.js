3693c826edb126503266362b78ef33dfba872be2
RyanGuitar <ryanelsguitar@gmail.com>
2021-04-22 13:00:08 +0200

    hide secret key

--- pages/location.js
+++ pages/location.js
@@ -2,13 +2,13 @@ import styles from './location.module.css'
 
 export default function Location (){
     const alt = "map Blougans B&B";
-    const src = "https://www.google.com/maps/embed/v1/place?key=AIzaSyCUWvaDWXVPmirhVlLIVH523Gtct-KWkWY&q=17+Blougans+St,+Perlemoenbaai,+Gansbaai";
+    
     return (
         <iframe
         id="map"
         alt= {alt}
         className={styles.map}
-        src= {src}>
+        src= {process.env.customKey}>
       </iframe>
     )
 }
