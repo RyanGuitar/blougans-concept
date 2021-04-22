6cb2c4c10bd198a33a85a3d634b14f3d726ffc4b
RyanGuitar <ryanelsguitar@gmail.com>
2021-04-22 12:35:38 +0200

    added google map to location

--- pages/location.js
+++ pages/location.js
@@ -1,9 +1,14 @@
 import styles from './location.module.css'
 
 export default function Location (){
+    const alt = "map Blougans B&B";
+    const src = "https://www.google.com/maps/embed/v1/place?key=AIzaSyCUWvaDWXVPmirhVlLIVH523Gtct-KWkWY&q=17+Blougans+St,+Perlemoenbaai,+Gansbaai";
     return (
-        <div className={styles.container}>
-            <div className={styles.header}>Location</div>
-        </div> 
+        <iframe
+        id="map"
+        alt= {alt}
+        className={styles.map}
+        src= {src}>
+      </iframe>
     )
 }
